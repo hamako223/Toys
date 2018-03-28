67b98fd334b7a9195a2c554879c44cecbd3c3207
HAMAK <hamak223@outlook.jp>
2018-03-28 13:39:36 +0900

    modified all

--- CountDownTimer.js
+++ CountDownTimer.js
@@ -6,19 +6,20 @@ function dateCounter(){
     //カウントダウンしたい日時を設定
     const targetDate = new Date("2018/5/27 10:00:00");
     //日数の計算
-    const dayCalc = Math.ceil((targetDate - currentDate)/(1000*60*60*24)-1);
-    const ms = (targetDate - currentDate);
-    if(ms >= 0){
+    const betweenDay = (targetDate - currentDate);
+    const dayCalc = Math.ceil((betweenDay)/(1000*60*60*24)-1);
+    if(betweenDay>=0){
       //時間を取得
-      const hour = ('0' + Math.floor(ms/(60*60*1000)) % 24).slice(-2);
+      const hour = ('0'+Math.floor(betweenDay/(60*60*1000))%24).slice(-2);
       //分を取得
-      const minute = ('0' + Math.floor((ms%(24*60*60*1000))/(60*1000))%60).slice(-2);
-      //廟を取得
-      const second = ('0' + Math.round((ms%(24*60*60*1000))/1000)%60%60).slice(-2);
+      const minute = ('0'+Math.floor((betweenDay%(24*60*60*1000))/(60*1000))%60).slice(-2);
+      //秒を取得
+      const second = ('0'+Math.round((betweenDay%(24*60*60*1000))/1000)%60%60).slice(-2);
       //ミリ秒を取得
-      const mili = ('0' + Math.floor((ms%(24*60*60*1000))/10)%100).slice(-2);
+      const mili = ('0'+Math.floor((betweenDay%(24*60*60*1000))/10)%100).slice(-2);
       //HTML上に出力
-      document.getElementById("counter").innerHTML ="<p class=\"counterText\">高槻キャンパス祭まで"+ "<br>残り" + dayCalc + "日" + hour + "時間" + minute + "分" + second + "秒"+mili+"</p>";
+      document.getElementById("counter").innerHTML
+      ="<p class=\"counterText\">高槻キャンパス祭まで"+"<br>残り"+dayCalc+"日"+hour+"時間"+minute+"分"+second+"秒"+mili+"</p>";
 
       if((hour == 0)&&(minute == 0)&&(second == 0)){
         clearInterval(timer);
